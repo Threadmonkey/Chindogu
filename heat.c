cbfdbf4a5cc042bfb4e9deb9946fc49045c7e9f4
Alexander Vondrous <alexander@vondrous.de>
2015-08-14 15:48:52 +0200

    Added open source licence to enable usage in all project, which need more heat ;-)

--- heat.c
+++ heat.c
@@ -1,6 +1,29 @@
 #include <omp.h>
-
 /*
+The MIT License (MIT)
+
+Copyright (c) 2015 Alexander Vondrous
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+# The Heat Chindogu
+
 The heat chindogu is application for your laptop to convert electric energy
 into heat, which is especially usefull in the winter season during commute
 in a train to warm your fingers.
@@ -31,6 +54,9 @@ Programming information
  - A feature would be to also use your GPU to produce even more heat.
 
 */
+
+#include <omp.h>
+
 int main (int argc, char *argv[]) {
   int i;
 
